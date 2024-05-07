95e9c27e503a114c01bdaba5ce40d1fc54eeda1e
Jaime Hernández Bravo <jaime@MacBook-Pro.local>
2024-05-07 17:23:00 +0200

    Filter user ratings by 3 customizable parameters

--- get_user_rankings.py
+++ get_user_rankings.py
@@ -1,22 +1,31 @@
 import requests
-import os
-import sys
 from bs4 import BeautifulSoup
 import pandas as pd
 
-ezentis_identifier = '32237'
+## identifier should be a console configurable input parameter
+identifier = '32237' # EZENTIS: 32237
 
-rankings_url = f'https://investing.com/common/sentiments/sentiments_ajax.php?action=get_user_rankings_bulk_records&item_ID={ezentis_identifier}&sentimentsBulkCount='
+## Rankings url, in the form of https://investing.com/common/sentiments/sentiments_ajax.php?action=get_user_rankings_bulk_records&item_ID=32237&sentimentsBulkCount=0
+## Where sentimentsBulkCount is a paginator that groups 50 users per page.
+## Fixed param.
+rankings_url = f'https://investing.com/common/sentiments/sentiments_ajax.php?action=get_user_rankings_bulk_records&item_ID='
 
-def get_records():
+## Conditions we need to apply to a user in order to trust the predictions made.
+win_percentage = 80
+number_of_predictions = 10
+variation_percentage = 10
+
+## Function that receives the identifier or symbol of a company and returns all the users that made predictions
+def get_user_ranking(identifier):
 
     iterator = 0
     response = ''
     records_left = True
+    header_data = [['Rango', 'Usuario', 'Total',	'Cerrados',	'Ganadores','Gan. %','% Var.']]
 
     while records_left:
         
-        rankings_bulk_records = requests.get(f'{rankings_url}{iterator}')
+        rankings_bulk_records = requests.get(f'{rankings_url}{identifier}&sentimentsBulkCount={iterator}')
 
         if rankings_bulk_records.content == b'':
             records_left = False
@@ -24,25 +33,47 @@ def get_records():
             response += rankings_bulk_records.content.decode()
             iterator += 1
     
-    return response
+    table_data = [[cell.text for cell in row("td")]
+                        for row in BeautifulSoup(response, "html.parser")("tr")]
 
-def main (iterator: int):
+    rankings = header_data + table_data
 
-        header_data = [['Rango', 'Usuario', 'Total',	'Cerrados',	'Ganadores','Gan. %','% Var.']]
+    rankings_list = pd.DataFrame(rankings[1:], columns=rankings[0])
 
-        records = get_records()
+    rankings_list = rankings_list.astype({'Total': 'int32', 'Cerrados': 'int32', 'Ganadores': 'int32'})
 
-        table_data = [[cell.text for cell in row("td")]
-                        for row in BeautifulSoup(records, "html.parser")("tr")]
+    rankings_list['% Var.'] = rankings_list['% Var.'].str.replace('%', '', regex=False).astype(float)
+    rankings_list['Gan. %'] = rankings_list['Gan. %'].astype(float)
 
-        rankings = header_data + table_data
+    return rankings_list
 
-        rankings_list = pd.DataFrame(rankings[1:], columns=rankings[0])
+def apply_trust_conditions(rankings_list,  win_percentage, number_of_predictions, variation_percentage):
 
-        print(rankings_list)
+    adjusted_rankings = rankings_list[rankings_list['Gan. %'] > win_percentage]
+    adjusted_rankings = adjusted_rankings[adjusted_rankings['Total'] > number_of_predictions]
+    adjusted_rankings = adjusted_rankings[adjusted_rankings['% Var.'] > variation_percentage]
 
-        iterator += 1
+    return adjusted_rankings
+def main ():
+
+    rankings_list = get_user_ranking(identifier)
+
+    trusted_users = apply_trust_conditions(rankings_list,  win_percentage, number_of_predictions, variation_percentage)
+
+    print('-----------------------')
+
+    print('ADJUSTED RANKING OF USERS THAT MADE PREDICTIONS ON SYMBOL :', identifier)
+    print('PARAMETERS:')
+    print('WON % RATE :  ', win_percentage)
+    print('TOTAL NUMBER OF PREDICTIONS : ', number_of_predictions)
+    print('SUBYACENT % VARIATION RATE : ', win_percentage)
+
+    print('-----------------------')
+
+    print(trusted_users)
+
+    print('Total of users : ', len(trusted_users) )
 
 if __name__ == '__main__' :
-    main(1)
+    main()
     
